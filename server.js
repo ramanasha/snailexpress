5077224c4a4ab0e5347aff8009e4da07d131ec51
Steven Xu <s@stevenxu.xyz>
2017-06-24 18:09:42 +0000

    move orders api to server.js

--- server.js
+++ server.js
@@ -137,7 +137,108 @@ app.get("/checkout", (req, res) => {
     .catch((err) => {
       res.status(500).json({ error: err.message });
     });
+});
 
+app.post("/", (req, res) => {
+  if (!req.body) {
+    res.status(400).json({ error: 'invalid request: no data in POST body'});
+    return;
+  }
+
+  let items = req.body.order.items;
+  let specialRequests = req.body.order.special_requests;
+  let restaurantId = req.body.order.restaurant_id;
+  let itemsIds = [];
+  let payment = req.body.payment;
+  let paymentType = req.body.payment.type;
+  let name = null;
+  let phone = null;
+  let cardNo = null;
+  let cardCsc = null;
+  let cardExpiry = null;
+  let email = "";
+  let min = req.body.order.min;
+
+  if (paymentType === 'credit') { // card
+    name = payment.credit.name;
+    phone = payment.credit.phone;
+    cardNo = payment.credit.card_no;
+    cardCsc = payment.credit.card_csc;
+    cardExpiry = payment.credit.card_expiry;
+  } else if (paymentType === 'debit') {
+    name = payment.debit.name;
+    phone = payment.debit.phone;
+  } else if (paymentType === 'store') {
+    name = payment.store.name;
+    phone = payment.store.phone;
+  }
+
+  // extract all item ids
+  for (let idx in items) {
+    itemsIds.push(items[idx].id);
+  }
+
+  // get price infomation
+  InventoryDataHelper.getPriceByIds(itemsIds)
+  .then((result) => {
+    // add price to item object
+    for (let idx in items) {
+      for (let dbIdx in result) {
+        if (items[idx].id === result[dbIdx].id) {
+          items[idx].price = result[dbIdx].price;
+          break;
+        }
+      }
+    }
+
+    return items;
+  }).then((items) => {
+    // caculate total price
+    let totalPrice = items.reduce((total, current) => {
+      return Number(total.price * total.qty) + Number(current.price * current.qty);
+    });
+
+    let timeToComplete = new Date();
+    timeToComplete.setMinutes(timeToComplete.getMinutes() + min);
+
+    let order = {
+      status: 'incomplete',
+      start_timestamp: new Date(),
+      total_price: totalPrice,
+      special_requests: specialRequests,
+      restaurant_id: restaurantId,
+      time_to_complete: timeToComplete
+    };
+
+    let payment = {
+      type: paymentType,
+      card_no: cardNo,
+      card_csc: cardCsc,
+      card_expiry: cardExpiry
+    };
+
+    let customer = {
+      phone: phone,
+      email: email
+    };
+
+    let orderItems = [];
+
+    for (let idx in items) {
+      let data = {
+        inventory_id: items[idx].id,
+        qty: items[idx].qty
+      };
+      orderItems.push(data);
+    }
+
+    // insert order to table
+    return OrderDataHelper.insertOrder(order, customer, orderItems, payment);
+  }).then(() => {
+    res.status(201).send();
+  }).catch((err) => {
+    res.status(500).json({ error: err.message });
+  });
 });
 
 //login and registration
