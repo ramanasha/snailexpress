2de0edb0a0c2995078a38be4e2bf996d03843d1b
Steven Xu <s@stevenxu.xyz>
2017-06-25 21:28:33 +0000

    add restaurant knex seed

---
+++ db/seeds/restaurants.js
@@ -0,0 +1,18 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('location').del()
+    .then(knex('restaurants').del())
+    .then(function () {
+      return Promise.all([
+        // Inserts seed entries
+        knex('location').insert({id: 1, street: '123 ABC Street', latitude: '32"', longitude: '12"'})
+      ]);
+    })
+    .then(function () {
+      return Promise.all([
+        // Inserts seed entries
+        knex('restaurants').insert({id: 1, name: 'Original Restaurant', phone: '3217236713', location_id: 1})
+      ]);
+    });
+};
