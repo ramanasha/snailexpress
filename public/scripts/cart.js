671281194c3a4cc34915d7846b86058ce2ffdd08
Rose Wan <rose.wan@me.com>
2017-06-25 22:25:04 -0400

    add spinner to cart.js and inside maps.ejs - loads too fast to see spinner

--- public/scripts/cart.js
+++ public/scripts/cart.js
@@ -48,38 +48,6 @@ function removeItem (inventoryId) {
   Cookies.set('cart', JSON.stringify(cart));
 }
 
-// //http://spin.js.org/
-// function loadingSpinner () {
-//   var opts = {
-//     lines: 13 // The number of lines to draw
-//   , length: 28 // The length of each line
-//   , width: 14 // The line thickness
-//   , radius: 42 // The radius of the inner circle
-//   , scale: 1 // Scales overall size of the spinner
-//   , corners: 1 // Corner roundness (0..1)
-//   , color: '#000' // #rgb or #rrggbb or array of colors
-//   , opacity: 0.25 // Opacity of the lines
-//   , rotate: 0 // The rotation offset
-//   , direction: 1 // 1: clockwise, -1: counterclockwise
-//   , speed: 1 // Rounds per second
-//   , trail: 60 // Afterglow percentage
-//   , fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
-//   , zIndex: 2e9 // The z-index (defaults to 2000000000)
-//   , className: 'spinner' // The CSS class to assign to the spinner
-//   , top: '50%' // Top position relative to parent
-//   , left: '50%' // Left position relative to parent
-//   , shadow: false // Whether to render a shadow
-//   , hwaccel: false // Whether to use hardware acceleration
-//   , position: 'absolute' // Element positioning
-//   }
-// var target = document.getElementById('foo')
-// var spinner = new Spinner(opts).spin(target);
-//   $('#mySpinner').addClass('spinner');
-//   success: (data) => {
-//     //the spinner will disappear when the data is successfully shown on page
-//     $('#mySpinner').removeClass('spinner');
-// }
-
 //Shows cart items
 function renderCart() {
   var cart = getCart();
@@ -93,8 +61,7 @@ function renderCart() {
 
   $('#cart').children().remove();
 
-  $('#cart').html('<div>Loading...</div>');
-  // loadingSpinner();
+  $('#cart').html('<div class="spinner"></div>');
 
   var allItems = Promise.all(cart.map((cartItem) => $.get("/api/inventories/" + cartItem.inventoryId)));
   allItems.then((items) => {
