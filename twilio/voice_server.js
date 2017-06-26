95161a9253db7b894c694251b7599f84802d3a75
Steven Xu <s@stevenxu.xyz>
2017-06-26 07:25:43 +0000

    checkpoint for restaurant phone call

---
+++ twilio/voice_server.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const express  = require('express');
+const twilio = require('twilio');
+
+module.exports = (OrderHelper, CustomerHelper, SMSHelper, port) => {
+  const app = express();
+  
+  // [twilio/orders/:id/inform_restaurant]
+  app.get("/twilio/orders/:id/inform_restaurant", (req, res) => {
+    //Create TwiML response
+    OrderHelper.getOrderById(req.params.id)
+    .then(([order]) => {
+      const customer = CustomerHelper.getCustomerById(order.customer_id);
+      const order_items = OrderHelper.getOrderItems(order.order_id);
+      return Promise.all([order, customer, order_items]);
+    })
+    .then(([order, customer, order_items]) => {
+      let twiml = new twilio.twiml.VoiceResponse();
+      twiml.say(`You have a new order. ${customer.name} ordered ${order_items.length} menu items.`);
+      
+      order_items.forEach((item) => {
+        twiml.say(`${item.qty} of ${item.weight} pound ${item.name} packages.`);
+      });
+      
+      res.writeHead(200, {'Content-Type': 'text/xml'});
+      res.end(twiml.toString());
+      
+    });
+  });
+
+  app.listen(port, () => {
+    console.log("Twilio server is listening on port " + port);
+  });
+  
+  return app;
+};
